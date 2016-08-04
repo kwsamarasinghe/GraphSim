05913fe9ca05fe9c975cf6c1b181d497a5a68985
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2016-08-04 20:08:33 +0200

    Add some logging messages

--- src/cui/tcs/graphsim/rest/GraphServer.java
+++ src/cui/tcs/graphsim/rest/GraphServer.java
@@ -1,5 +1,6 @@
 package cui.tcs.graphsim.rest;
 
+import org.apache.commons.logging.impl.Log4JLogger;
 import org.restlet.Component;
 import org.restlet.data.Protocol;
 
@@ -7,22 +8,38 @@ import cui.tcs.graphsim.graph.ConnectivityGraph;
 
 public class GraphServer  {
 
+	//Graph
 	private ConnectivityGraph graph;
 
+	//REST component
+	private Component component;
+	
+	private Log4JLogger logger;
+	
 	public GraphServer(ConnectivityGraph graph) {
 		this.graph = graph;
+		logger=new Log4JLogger();
 	}
 
 	public boolean start() {
 		try {
-			Component component = new Component();
+			component = new Component();
 			component.getServers().add(Protocol.HTTP, 8111);
 			component.getDefaultHost().attach("/graph", new GraphRouter(graph));
 			component.start();
-			System.out.println("HTTP Listner started successfully");
+			logger.debug("graph Server Started on port 8111");
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean stop(){
+		try {
+			component.stop();
+			return true;
+		} catch (Exception e) {
 			return false;
 		}
 	}
--- src/cui/tcs/graphsim/simulator/Simulator.java
+++ src/cui/tcs/graphsim/simulator/Simulator.java
@@ -1,26 +1,47 @@
 package cui.tcs.graphsim.simulator;
 
+import cui.tcs.graphsim.engine.Engine;
 import cui.tcs.graphsim.graph.instances.geometric.RandomGeometricGraphImpl;
 import cui.tcs.graphsim.rest.GraphServer;
+import cui.tcs.graphsim.samples.SamplePacket;
 
-public class Simulator{
+public class Simulator {
 
-	private RandomGeometricGraphImpl randomGeometricGraph;
-	
-	public Simulator(){
-		randomGeometricGraph=new RandomGeometricGraphImpl(100, 50, 50, 10);
-		
-		//Starts the REST service
-		GraphServer graphServer=new GraphServer(randomGeometricGraph);
-		graphServer.start();
-		
+	private GraphServer graphServer;
+
+	private Engine engine;
+
+	private RandomGeometricGraphImpl<SamplePacket> randomGeometricGraph;
+
+	public Simulator() {
+		randomGeometricGraph = new RandomGeometricGraphImpl<SamplePacket>(100, 50, 50, 10);
 	}
-	
-	
-	public static void main(String args[]){
-		Simulator simulator=new Simulator();
+
+	public void start() {
+		// Starts the REST service
+		graphServer = new GraphServer(randomGeometricGraph);
+		graphServer.start();
+
+		//Starts the engine
+		Engine engine = new Engine(randomGeometricGraph);
+		engine.start();
+
+	}
+
+	public void stop() {
+		// Stops the server
+		graphServer.stop();
+	}
+
+	public static void main(String args[]) {
+		Simulator simulator = new Simulator();
+		simulator.start();
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				simulator.stop();
+			}
+		});
 	}
 
 }
-
-
