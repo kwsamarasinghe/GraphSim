34d31730d7d47c2c239568926c7c3c403a5eb287
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2016-08-10 19:53:08 +0200

    Pakcet handling process added

--- src/cui/tcs/graphsim/samples/RandomWalkProcess.java
+++ src/cui/tcs/graphsim/samples/RandomWalkProcess.java
@@ -1,11 +1,15 @@
 package cui.tcs.graphsim.samples;
 
+import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import com.tinkerpop.blueprints.Vertex;
 
 import cui.tcs.graphsim.engine.NodeProcess;
 import cui.tcs.graphsim.engine.Packet;
+import cui.tcs.graphsim.engine.PacketForwarder;
+import cui.tcs.graphsim.engine.SimulationContext;
+import cui.tcs.graphsim.graph.instances.geometric.GeometricNodeImpl;
 
 /**
  * Simple random walk node process just forwards the packets to a neighboring
@@ -17,14 +21,17 @@ import cui.tcs.graphsim.engine.Packet;
 public class RandomWalkProcess implements NodeProcess {
 
 	// Packet buffer
-	private ConcurrentLinkedQueue<Packet> buffer;
+	private ConcurrentLinkedQueue<Packet> packetQueue;
 
 	//Associated node
 	private Vertex vertex;
 	
-	public RandomWalkProcess(Vertex vertex){
+	private SimulationContext context;
+	
+	public RandomWalkProcess(Vertex vertex, SimulationContext context){
 		this.vertex=vertex;
-		buffer=new ConcurrentLinkedQueue<Packet>();
+		this.context=context;
+		packetQueue=new ConcurrentLinkedQueue<Packet>();
 	}
 	
 	/**
@@ -36,15 +43,26 @@ public class RandomWalkProcess implements NodeProcess {
 		// sample process which generates a packet and performs a random
 		// walk
 		if ((int) vertex.getId() == 0) {
-
+			//Assumes we get the destination 
+			Random random=new Random();
+			int destinationID=random.nextInt(10);
+			
+			Packet packet=new Packet();
+			packet.setDesnation(destinationID);
+			
+			PacketForwarder forwarder=context.getForwarder();
+			forwarder.sendPacket(packet);
 		}
 
 	}
 
 	public void handlePacket(Packet packet) {
-		// TODO Auto-generated method stub
 		System.out.println("Receiving a packet from " + packet.getSource() + " to" + packet.getDestination());
-
+		packetQueue.add(packet);
+	}
+	
+	public Vertex getNode(){
+		return vertex;
 	}
 
 }
@@ -54,15 +72,43 @@ public class RandomWalkProcess implements NodeProcess {
  */
 class PacketHandler implements Runnable{
 	
-	ConcurrentLinkedQueue<Packet> buffer;
+	ConcurrentLinkedQueue<Packet> packetQueue;
 	
-	public PacketHandler(ConcurrentLinkedQueue<Packet> buffer){
-		this.buffer=buffer;
+	SimulationContext context;
+	
+	RandomWalkProcess mainProcess;
+	
+	public PacketHandler(RandomWalkProcess randomWalkProcess, ConcurrentLinkedQueue<Packet> packetQueue,SimulationContext context){
+		this.mainProcess=randomWalkProcess;
+		this.packetQueue=packetQueue;
+		this.context=context;
 	}
 	
 	public void run(){
-		while(!buffer.isEmpty()){
+		PacketForwarder forwarder=context.getForwarder();
+		Random random=new Random();
+		
+		
+		while(!packetQueue.isEmpty()){
+			Packet packet=packetQueue.remove();
+			int destination=packet.getDestination();
 			
+			Vertex node=mainProcess.getNode();
+			Vertex nextNode=null;
+			Iterable<Vertex> neighborNodes=node.getVertices(null, "");
+			for(Vertex neighbor: neighborNodes){
+				if(Math.random()>0.5){
+					nextNode=neighbor;
+					break;
+				}
+			}
+			
+			if(nextNode==null){
+				nextNode=neighborNodes.iterator().next();
+			}
+			
+			packet.setNextHop((int)nextNode.getId());
+			forwarder.sendPacket(packet);
 		}
 	}
 }
