340e7e14db9de8dc538cace9ce4603767b25483f
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2016-09-07 22:56:21 +0200

    Graph update resource

---
+++ src/cui/tcs/graphsim/rest/resources/GraphUpdateResource.java
@@ -0,0 +1,12 @@
+package cui.tcs.graphsim.rest.resources;
+
+import org.junit.runner.Request;
+import org.restlet.Response;
+import org.restlet.Restlet;
+
+public class GraphUpdateResource extends Restlet{
+
+	public void handle(Request request, Response response){
+		
+	}
+}
